8b6438a14b3b23587a78a720d029abcd1ba7e874
Romy Furtado <rofrtd@gmail.com>
2022-06-30 18:57:36 -0300

    rename import

--- src/features/game/expansion/Land.tsx
+++ src/features/game/expansion/Land.tsx
@@ -8,7 +8,7 @@ import { useActor } from "@xstate/react";
 import { Context } from "../GameProvider";
 import { getTerrainImageByKey } from "../lib/getTerrainImageByKey";
 import { MapPlacement } from "./components/MapPlacement";
-import { Pebble } from "./components/resources/Pebble";
+import { Pebble } from "./components/resources/pebble";
 
 export const Land: React.FC = () => {
   const { gameService } = useContext(Context);
